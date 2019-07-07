01f8743b13ac0e065e6f3ce92b775da74bc20a61
Egemen Guray <egemenguray@hotmail.com>
2019-07-07 18:59:15 -0400

    Add files via upload

---
+++ Products/App_Start/RouteConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Products
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.MapRoute("ProductList","list/product", new {controller="Product",action="GetList" });
+            
+        }
+    }
+}
---
+++ Products/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using Culinars.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Products.Controllers
+{
+    public class ProductController : Controller
+    {
+        public JsonResult GetList()
+        {
+            var model = new List<Product>();
+            model.Add(new Product() { Id = 1, Name = "Water" });
+            model.Add(new Product() { Id = 2, Name = "Potato" });
+            model.Add(new Product() { Id = 3, Name = "Tomato" });
+
+
+            return Json(model, JsonRequestBehavior.AllowGet);
+        }
+       
+    }
+}
